841b51959f018a3b881e598ba85b2ff49c2a31a6
Lukeeeeee <lukedong123@gmail.com>
2018-10-24 14:15:24 +0800

    generate basic dataset

--- src/generate_attributes.py
+++ src/generate_attributes.py
@@ -3,10 +3,20 @@ import json
 import os
 
 
+def load_json(file_path):
+    with open(file_path, 'r') as f:
+        return json.load(f)
+
+
+def export_json(file_path, dict):
+    with open(file_path, 'w') as f:
+        json.dump(dict, f, indent=4, sort_keys=True)
+
+
 class AttributesGenerator(object):
     def __init__(self, attr_config_file_path, sample_number=10):
-        with open(attr_config_file_path, 'r') as f:
-            self.attr_config_dict = json.load(f)
+
+        self.attr_config_dict = load_json(file_path=attr_config_file_path)
 
         self.dataset = []
 
@@ -32,34 +42,55 @@ class AttributesGenerator(object):
         return int(res)
 
     @staticmethod
+    def _generate_save_time(range, step):
+        assert range[0] == 0
+        range = np.arange(range[0], range[1], step=step)
+        min_val = range[0]
+        max_val = range[-1]
+        zero_prob = np.random.rand()
+        if zero_prob < 0.8:
+            return 0
+        else:
+            res = int(np.clip(np.random.normal(loc=(max_val + min_val) / 2.0, scale=1.0),
+                              a_min=min_val,
+                              a_max=max_val))
+            return res
+
+    @staticmethod
     def generate_one_sample(attr_config_dict):
         sample_dict = {}
         for key, val in attr_config_dict.items():
-            if val['TYPE'] == 'CONTINUOUS':
-                mean = val['MEAN'] if "MEAN" in val else None
-                std = val['STD'] if "STD" in val else None
-                res = AttributesGenerator._generate_continuous_attributes(range=list(val['RANGE']),
-                                                                          mean=mean,
-                                                                          std=std)
+            if key == 'SAVING_TIME':
+                res = AttributesGenerator._generate_save_time(range=list(val['RANGE']),
+                                                              step=1)
+            else:
+                if val['TYPE'] == 'CONTINUOUS':
+                    mean = val['MEAN'] if "MEAN" in val else None
+                    std = val['STD'] if "STD" in val else None
+                    res = AttributesGenerator._generate_continuous_attributes(range=list(val['RANGE']),
+                                                                              mean=mean,
+                                                                              std=std)
 
-            elif val['TYPE'] == 'ORDINAL':
-                weight = val['WEIGHT'] if 'WEIGHT' in val else None
-                res = AttributesGenerator._generate_ordinal_attributes(range=val['RANGE'],
-                                                                       step=val['STEP'],
-                                                                       weight=weight)
-
-            elif val['TYPE'] == 'CATEGORICAL':
-                weight = val['WEIGHT'] if 'WEIGHT' in val else None
-                res = AttributesGenerator._generate_categorical_attributes(val_list=list(val['VALUE']),
+                elif val['TYPE'] == 'ORDINAL':
+                    weight = val['WEIGHT'] if 'WEIGHT' in val else None
+                    res = AttributesGenerator._generate_ordinal_attributes(range=val['RANGE'],
+                                                                           step=val['STEP'],
                                                                            weight=weight)
 
-            else:
-                raise ValueError
+                elif val['TYPE'] == 'CATEGORICAL':
+                    weight = val['WEIGHT'] if 'WEIGHT' in val else None
+                    res = AttributesGenerator._generate_categorical_attributes(val_list=list(val['VALUE']),
+                                                                               weight=weight)
+
+                else:
+                    raise ValueError
             sample_dict[key] = res
         return sample_dict
 
 
 if __name__ == '__main__':
     from dataset import DATASET_PATH
-    a = AttributesGenerator(attr_config_file_path=os.path.join(DATASET_PATH, 'attributes.json'))
-    print(json.dumps(a.dataset, indent=4, sort_keys=True))
+
+    a = AttributesGenerator(attr_config_file_path=os.path.join(DATASET_PATH, 'attributes_config.json'),
+                            sample_number=10000)
+    export_json(file_path=os.path.join(DATASET_PATH, 'attr_dataset.json'), dict=a.dataset)
--- src/generate_labels.py
+++ src/generate_labels.py
@@ -1,1 +1,43 @@
+import numpy as np
+import json
+import os
+from src.generate_attributes import export_json, load_json
+from dataset import DATASET_PATH
 
+
+class LabelGenerator(object):
+    def __init__(self, attr_dataset, label_config_file_path):
+        self.attr_dataset = attr_dataset
+        self.label_config = load_json(file_path=label_config_file_path)
+        self.final_dataset = []
+
+    def process_data(self):
+        for sample in self.attr_dataset:
+            label, new_sample = self._compute_label_with_one_sample(sample=sample)
+            self.final_dataset.append(new_sample)
+        export_json(file_path=os.path.join(DATASET_PATH, 'attr_label_dataset.json'), dict=self.final_dataset)
+
+    def _compute_label_with_one_sample(self, sample):
+        # Compute each attribute's contribution to each label,
+        # Suppose we have m attributes, and n labels,
+        # So we use a m x n matrix to
+        # compute the sum contribution over all attributes of one sample w.r.t. each label
+        # The label with maximum value will be the ground truth of this sample
+        label_score_list = np.zeros(len(self.label_config['CLASS_DICT']))
+        for key, val in sample.items():
+            res = np.array(self.label_config['CLASS_WEIGHT_WITH_ATTR'][key], dtype=np.float32) * val
+            label_score_list += res
+        label = np.argmax(label_score_list)
+        sample['LABEL'] = int(label)
+        for key, val in self.label_config['CLASS_DICT'].items():
+            if val == label:
+                sample['LABEL_NAME'] = key
+        # TODO
+        # add random noise for label
+        return label, sample
+
+
+if __name__ == '__main__':
+    a = LabelGenerator(attr_dataset=load_json(file_path=os.path.join(DATASET_PATH, 'attr_dataset.json')),
+                       label_config_file_path=os.path.join(DATASET_PATH, 'label_config.json'))
+    a.process_data()

b8be739a87fdb45f6fc55b70b9e8e27070b6b3ee
Lukeeeeee <lukedong123@gmail.com>
2018-10-24 12:49:42 +0800

    finish attr generation

--- README.md
+++ README.md
@@ -20,29 +20,33 @@ Use football player statistical features like:
 ```
 Continuous Attributes:
 
-Speed (normal distributed on label, no impact on label)
-Height (small impact on label, treat as categorical)
-Weight (normal distributed on label, no impact on label, treat as categorical)
-Average Scores number (Higher for offense)
-Average Assistance number (Higher for offense)
-Average Stealing number (Higher for defense)
-Average Tacking number (Higher for defense)
-Average Fouls number (Higher for defense, redundent attributes of Yellow/Red Card number)
+Speed (normal distributed on label, no impact on label) (10s - 20s for 100m]
+Height (small impact on label, treat as categorical) (140cm - 200cm)
+Weight (normal distributed on label, no impact on label, treat as categorical) [70kg - 100kg]
+Average Scores number Per Game (Higher for offense)  [0, 6]
+Average Assistance number (Higher for offense) [0, 6]
+Average Stealing number (Higher for defense) [0, 20]
+Average Tacking number (Higher for defense) [0, 20]
+Average Fouls number (Higher for defense, redundent attributes of Yellow/Red Card number) [0, 20]
 
 Discrete Attributes:
 
 Game playing time
-Saving time (For goal keeper only, which is a quite special attribute)
-Age (no impact on label)
-Nation (no impact on label)
-Yellow/Red Card number (Higher for defense, redundent attributes of Average Fouls number)
+Saving time (For goal keeper only, which is a quite special attribute) (0, 1, 2, ... 10)
+Age (no impact on label) (16..40)
+Nation (no impact on label) (A, B, C, D)
+Yellow/Red Card number in recent 10 games (Higher for defense, redundent attributes of Average Fouls number) (0, 1, ...20) 
 ```
 
 to classify football player into different roles:
-
 ```
 1. Goalkeeper
 2. Defenders 
 3. Midfielders
 4. Attackers
 ```
+
+### Label Generating Method
+For attributes, considering its contribution or weight to different label. 
+
+For continuous data, first scale it into `[0, 1]`, 
--- src/generate_attributes.py
+++ src/generate_attributes.py
@@ -1,16 +1,65 @@
 import numpy as np
+import json
+import os
 
 
 class AttributesGenerator(object):
-    def __init__(self):
+    def __init__(self, attr_config_file_path, sample_number=10):
+        with open(attr_config_file_path, 'r') as f:
+            self.attr_config_dict = json.load(f)
+
         self.dataset = []
-        pass
 
-    def generate_categorical_attributes(self):
-        pass
+        for i in range(sample_number):
+            self.dataset.append(self.generate_one_sample(attr_config_dict=self.attr_config_dict))
 
-    def generate_continuous_attributes(self):
-        pass
+    @staticmethod
+    def _generate_categorical_attributes(val_list, weight=None):
+        res = np.random.choice(np.array(val_list), p=weight)
+        return int(res)
 
-    def generate_ordinal_attributes(self):
-        pass
+    @staticmethod
+    def _generate_continuous_attributes(range, mean=None, std=None):
+        if not mean:
+            return float(np.random.uniform(low=range[0], high=range[1]))
+        else:
+            return float(np.clip(np.random.normal(loc=mean, scale=std), a_min=range[0], a_max=range[1]))
+
+    @staticmethod
+    def _generate_ordinal_attributes(range, step, weight=None):
+        range = np.arange(range[0], range[1], step=step)
+        res = np.random.choice(range, p=weight)
+        return int(res)
+
+    @staticmethod
+    def generate_one_sample(attr_config_dict):
+        sample_dict = {}
+        for key, val in attr_config_dict.items():
+            if val['TYPE'] == 'CONTINUOUS':
+                mean = val['MEAN'] if "MEAN" in val else None
+                std = val['STD'] if "STD" in val else None
+                res = AttributesGenerator._generate_continuous_attributes(range=list(val['RANGE']),
+                                                                          mean=mean,
+                                                                          std=std)
+
+            elif val['TYPE'] == 'ORDINAL':
+                weight = val['WEIGHT'] if 'WEIGHT' in val else None
+                res = AttributesGenerator._generate_ordinal_attributes(range=val['RANGE'],
+                                                                       step=val['STEP'],
+                                                                       weight=weight)
+
+            elif val['TYPE'] == 'CATEGORICAL':
+                weight = val['WEIGHT'] if 'WEIGHT' in val else None
+                res = AttributesGenerator._generate_categorical_attributes(val_list=list(val['VALUE']),
+                                                                           weight=weight)
+
+            else:
+                raise ValueError
+            sample_dict[key] = res
+        return sample_dict
+
+
+if __name__ == '__main__':
+    from dataset import DATASET_PATH
+    a = AttributesGenerator(attr_config_file_path=os.path.join(DATASET_PATH, 'attributes.json'))
+    print(json.dumps(a.dataset, indent=4, sort_keys=True))
